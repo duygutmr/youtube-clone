9fc79498c55863d660eb584486ff9e1daadd7115
Duygu Temur <duygu@frank.com.tr>
2022-12-05 23:00:40 +0300

    add sidebar categories

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,30 @@
 import { Stack, Box, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+
+import { useState } from "react";
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" } }}></Box>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022
+        </Typography>
+      </Box>
     </Stack>
   );
 };
